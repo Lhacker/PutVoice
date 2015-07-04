eac789e5d68827483cd9e6ce166da5dd26d04559
Lhacker <starclown1@gmail.com>
2015-07-04 19:01:41 +0900

    add upload function

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,17 @@ import play.mvc.*;
 
 import views.html.*;
 
+import java.io.File;
+
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result upload() {
+      File file = request().body().asRaw().asFile();
+      return ok("uploaded file");
     }
 
 }
